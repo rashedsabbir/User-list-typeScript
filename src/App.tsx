760960a5ba98f399ae94c63266fe4e9d8a761849
rashedsabbir <rashedulhasan1996@gmail.com>
2022-08-15 22:35:37 +0600

    save values to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
+
 import './App.css';
 
-function App() {
+const App: React.FC=()=> {
+
+  interface UserInt{
+    name:string, 
+    age:string, 
+    job:string
+  }
+
+  const [usersState, setUsersState] = useState<{currentUser:UserInt}>({
+    currentUser:{
+      name: "",
+      age: "",
+      job: ""
+      
+    }
+  })
+
+  const onChangeHandler= (event : React.ChangeEvent<HTMLInputElement>) : void => {
+    console.log(event.target.value);
+    
+    setUsersState({
+      currentUser:{
+        ...usersState.currentUser,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+    console.log(usersState.currentUser);
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React with TypeScript</h1>
+      <form action="">
+        <label htmlFor="userName">Name:</label>
+        <input 
+        id='userName'
+        type="text"
+        name='name'
+        value={usersState.currentUser.name}
+        onChange={onChangeHandler}
+        />
+        <label htmlFor="userAge">Age:</label>
+        <input 
+        id='userAge'
+        type="number"
+        name='age'
+        value={usersState.currentUser.age}
+        onChange={onChangeHandler}
+        />
+        <label htmlFor="userJob">job:</label>
+        <input 
+        id='userJob'
+        type="text"
+        name='job'
+        value={usersState.currentUser.job} 
+        onChange={onChangeHandler}
+        />
+
+        <button type='submit'>Add User</button>
+      </form>
     </div>
   );
 }
